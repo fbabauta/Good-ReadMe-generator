46cdd60ddeb71f1b5365224f9e5954d755f6ddcd
francine babauta <francine.babauta@outlook.com>
2020-12-08 01:01:28 -0800

    fixed api.js file

--- utils/api.js
+++ utils/api.js
@@ -2,13 +2,14 @@ const axios = require('axios');
 
 const api = {
     async getUser(userResponses)   {
-        try { let response = await axios
+        try {
+            let response = await axios;
         
           // Sample URL: https://api.github.com/users/fbabauta
           .get('https://api.github.com/users/${userResponses.username}');
           return response.data;
           
-          } catch (error)   {
+          } catch (error)  {
               console.log(error);
           }
     }
